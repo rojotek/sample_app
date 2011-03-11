fe8d613f7a88044d05131f61dab856935a565213
Rob Dawson <robert.dawson@ephox.com>
2011-03-11 15:52:25 +1000

    Layouts worked out and made nice.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if !@title then
+      base_title
+    else
+      base_title +" | " + @title
+    end
+      
+  end
 end
