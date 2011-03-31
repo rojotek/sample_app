66e0688d52f0a21dac901024fcbd78081d53a9c7
Rob Dawson <robert.dawson@ephox.com>
2011-04-01 08:43:48 +1000

    got broken tests for the users controller.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
-  #render_views is required to make the html out
+  #render_views is required to make the actions perfom html output
   render_views
   
   def get_should_be_success(action, params=nil)
@@ -35,9 +35,40 @@ describe UsersController do
     
     it "should have a profile image" do
       get :show, id: @user
-      response.should have_selector('h1>img', class: "gravatar")
+      response.should have_selector('h1>img', class: "gravatar round")
     end
     
+  end
+  
+  describe "POST 'create'" do
+    describe 'failure' do
+       before(:each) do
+         @attr={name:"", email: "", password: "", password_confirmation: ""}
+       end
+       it "should not create a user" do
+         lambda do
+           post :create, user: @attr
+         end.should_not change(User, :count)
+       end
+       it "should have the right title" do
+         post :create, user: @attr
+         response.should have_selector("title", content: "Sign up")
+       end
+       it "should render the 'new' page" do
+         post :create, user: @attr
+         response.should render_template("new")
+       end
+    end
+    describe 'success' do
+      before(:each) do
+        @user = Factory(:user)
+      end
+      it "should be successful" do
+          post 'create', user: @user.attributes
+          response.should be_success
+      end
+      it "should create a user"
+    end
   end
   describe "GET 'new'" do
     it "should be successful" do
